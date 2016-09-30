ca2a8477bb1a056f4c88794467c304fa64a0e1e8
Ian Firkin <ian.firkin@gisinc.com>
2016-09-29 22:52:26 -0400

    Adds choco file

---
+++ choco.sh
@@ -0,0 +1,8 @@
+mkdir c:/develop
+choco install git -y
+choco install conemu -y
+choco install virtualbox -y
+choco install vagrant -y
+choco install packer -y
+choco install nodejs -y
+choco install webstorm -version 11.0.3 -y
